2d7cc3c6629fec2d07c41f29ef1a82208563ffb1
Aptenodyte <64998505+Aptenodyte@users.noreply.github.com>
2024-09-13 23:54:12 -0500

    Clean analog input data and graph TPS and RPM vs. Time

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("hello world my changes")
+import pandas as pd
+
+df = pd.read_csv('can-_ecu_762.csv')
+analog_columns = df.iloc[:, 1:9]
+df.drop(analog_columns.columns[analog_columns.nunique() <= 1], axis=1, inplace=True)
+df.to_csv('cleaned_data.csv')
+
+pd.options.plotting.backend = 'plotly'
+rpm = df.loc[:, ['timestamp', 'RPM']]
+tps = df.loc[:, ['timestamp', 'TPS']]
+
+rpm_graph = rpm.plot(title='RPM vs. Time', labels=dict(timestamp='Time', index='RPM'), x='timestamp')
+tps_graph = tps.plot(title='TPS vs. Time', labels=dict(timestamp='Time', index='TPS'), x='timestamp')
+
+rpm_graph.show()
+tps_graph.show()
