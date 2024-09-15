71d3c91268777745666f1b222965b45a80ab779c
seniph <seniphh@gmail.com>
2024-09-14 20:40:55 -0500

    Update main.py
    Added in code in order to remove columns that remain constant and plotted graphs for RPM vs. Time and TPS vs. Time.

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px 
+pd.options.plotting.backend = "plotly"
+
+dataFrame = pd.read_csv('can-_ecu_762.csv')
+editedDataFrame = dataFrame
+
+for col in dataFrame:
+
+    isUniqueRow = False
+    for row in range(len(dataFrame)):
+        
+        if (dataFrame.at[row, col] != dataFrame.at[0,col]):
+            isUniqueRow = True
+
+    if(isUniqueRow == False):
+        editedDataFrame = dataFrame.drop([col], axis=1)
+
+rpmGraph = px.line(editedDataFrame, x="timestamp", y="RPM", title="RPM vs. Time")
+tpsGraph = px.line(editedDataFrame, x="timestamp", y="TPS", title="TPS vs. Time")
+
+rpmGraph.show()
+tpsGraph.show()
