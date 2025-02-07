6eb163a0fc128d61cb9b9e0f3d4a1751294efed2
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-07 13:45:23 -0600

    Complete Onboarding(redo)

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
-print("hello world my changes")
+import plotly.express as px
+
+import pandas as pd
+
+csv = "datadump.csv"
+df = pd.read_csv(csv)
+
+x = [f"Analog Input #{x}" for x in range(3,9)]
+
+# print(x)
+print(df.drop(x, axis = 1))
+
+RPM = df["RPM"].tolist()
+TPS = df["TPS"].tolist()
+RPM_GRAPH = px.line(RPM) 
+TPS_GRAPH = px.line(TPS)
+
+ 
+# printing the figure instance
+RPM_GRAPH.show()
+TPS_GRAPH.show()
