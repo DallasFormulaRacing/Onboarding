a2de3c99b38cb1aac56bdb1133a1aee285959cf8
Daniel-Eenis <141870998+DanielEnis01@users.noreply.github.com>
2025-02-12 22:10:49 -0600

    Removed unchanging analog inputs from the csv data frame and made two graphs for TSM/timestamp and RPM/timestamp

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px
+
+
+df = pd.read_csv('DataDump.csv')
+
+removedCols = []
+for col in df.columns:
+    if 'Analog Input' in col and df[col].nunique() == 1:
+        removedCols.append(col)
+
+df = df.drop(columns=removedCols)
+
+print(f"Unchanging columns: {removedCols}")
+
+# RPM
+fig_rpm = px.line(df, x='timestamp', y='RPM', title='RPM / Time')
+fig_rpm.show()
+
+# TPS
+fig_tps = px.line(df, x='timestamp', y='TPS', title='TPS / Time')
+fig_tps.show()
