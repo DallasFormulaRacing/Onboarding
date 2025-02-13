3f4b92cdd5644bff91bf73c9d288766e3ef97153
Tbelkk <tylerbelkenyt@gmail.com>
2025-02-12 22:44:23 -0600

    Removed constant Analog Inputs and Graphed the RPM and TPS using plotly

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("hello world my changes")
+import pandas as pd
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+
+df = pd.read_csv("can-_ecu_762.csv")
+1
+for col in df.columns:
+    if 'Analog Input' in col and df[col].nunique() == 1:
+            df.drop(columns=col, inplace=True)
+            print(f"Removed constant columns: {col}")
+
+
+fig = make_subplots(rows=2, cols=1)
+
+fig.add_trace(
+    go.Scatter(x=df['timestamp'], y=df['RPM'], name="RPM/Time"), 
+    row=1, col=1
+    )
+
+fig.add_trace(
+    go.Scatter(x=df['timestamp'], y=df['TPS'], name="TPS/Time"), 
+    row=2, col=1
+    )
+
+fig.update_layout(title_text="RPM and TPS Over Time")
+
+fig.show()
