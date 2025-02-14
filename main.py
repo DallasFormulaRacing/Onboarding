96daba5e6be36a2914c7460098403974f5ceecf5
aymanhaque <muhd.ayman.haque@gmail.com>
2025-02-13 20:14:36 -0600

    Onboarding Fully Complete. Checks for analog inputs with no change in value and deletes them

--- main.py
+++ main.py
@@ -7,8 +7,21 @@ df = pd.read_csv(csv)
 
 x = [f"Analog Input #{x}" for x in range(3,9)]
 
+
+
+# .drop(x, axis = 1)
 # print(x)
-print(df.drop(x, axis = 1))
+analogInput = [x for x in df.columns if "Analog Input" in x]
+for i in analogInput:
+    switch = 0
+    for k,j in enumerate(df[i]):
+        if k == 0: continue
+        if j != df.loc[k-1,i] and not pd.isna(j+df.loc[k-1,i]):
+            switch = 1
+            break
+    if(switch == 0):    
+        df = df.drop(i, axis = 1)
+print(df)
 
 RPM = df["RPM"].tolist()
 TPS = df["TPS"].tolist()
@@ -18,5 +31,5 @@ TPS_GRAPH = px.line(y=TPS, x=timeStamp)
 
  
 # printing the figure instance
-RPM_GRAPH.show()
-TPS_GRAPH.show()
+# RPM_GRAPH.show()
+# TPS_GRAPH.show()
