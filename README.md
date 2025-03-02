1e713fe88a6ef0c8e2412fd516094fb0c8a92b13
AravNeroth <reacharav@outlook.com>
2025-03-02 00:28:06 -0600

    WE DID IT CHAT
    rahhhhh lowkey the plotting was kinda easy once i found out what plotly express was

--- README.md
+++ README.md
@@ -3,9 +3,9 @@
 Steps:
 - [X] Fork this repository into your account
 - [X] Remove all `Analog Input` columns with dead sensors. These are sensors whose values stay constant.
-- [ ] Graph the RPM, and TPS columns seperately (or in the same graph for fun lol)
-- [ ] Submit a PR back to this repository with your code
-- [ ] Send me (@sq.id on discord) a message with a link to your PR and why you want to join the club.
+- [X] Graph the RPM, and TPS columns seperately (or in the same graph for fun lol)
+- [X] Submit a PR back to this repository with your code
+- [X] Send me (@sq.id on discord) a message with a link to your PR and why you want to join the club.
 
 
 We have an open door policy, but require new members to follow basic coding structures to get into team projects.
--- main.py
+++ main.py
@@ -6,28 +6,33 @@ import pandas as pd
 import sqlalchemy
 import random
 import re
+import plotly.express as plotly
 
 '''
 made a random number generator bc i kept generating excel files to see if it worked
 and it kept crashing bc the name was the same so this just makes every file unique lol
 '''
 
+# sorting data
 rand_num = random.randint(0, 9999)
-file = "can-_ecu_762.xlsx"
+unsorted_file = "can-_ecu_762.xlsx"
 output = f"sorted{rand_num}.xlsx"
+
+# using the sorted data for graphs
+sorted_file = f"sorted{rand_num}"
 
 # locally process this using SQLite
 engine = sqlalchemy.create_engine("sqlite://", echo=False)
 
 # add data and make it processable
 sheet_name = "can-_ecu_762"
-df = pd.read_excel(file, sheet_name=sheet_name)
+df = pd.read_excel(unsorted_file, sheet_name=sheet_name)
 df.columns = df.columns.str.strip()
 
 # look for columns that match "Analog Input #1", "Analog Input #2", etc.
 analog_input_cols = [col for col in df.columns if re.match(r"Analog Input #\d+", col)]
 
-# using said sorted columns above, see which ones don't have changing data
+# using said sorted columns above, see which ones do    n't have changing data
 constant_cols = [col for col in analog_input_cols if df[col].nunique() == 1]
 
 # remove em 
@@ -44,6 +49,20 @@ with engine.connect() as conn:
 
 # new sorted file!
 final_df.to_excel(output, index=False)
-print(final_df)
-print(f"Successfully processed and outputted in file: sorted{rand_num}")
+print(f"Successfully processed and outputted in file: sorted{rand_num}.")
 
+# very convienet graphing
+fig = plotly.scatter(final_df, x="timestamp", y=["RPM", "TPS"], title="Scatter Plot of RPM & TPS over time")
+print(f"Successfully plotted RPM & TPS to a scatterplot.")
+
+fig2 = plotly.scatter(final_df, x="timestamp", y="RPM", title="Scatter Plot of RPM over time")
+print(f"Successfully plotted RPM to a scatterplot.")
+
+fig3 = plotly.scatter(final_df, x="timestamp", y="TPS", title="Scatter Plot of TPS over time")
+print(f"Successfully plotted TPS to a scatterplot.")
+
+print(f"Redirecting to browser to view.")
+
+fig.show()
+fig2.show()
+fig3.show()
