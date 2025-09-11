9750a5254bf296e2ba4a9af85effca8c2224afe0
Saaketh <113314505+srkwork@users.noreply.github.com>
2025-09-11 18:18:37 -0500

    Fixed the comments

--- main.py
+++ main.py
@@ -11,26 +11,17 @@ df = pd.read_csv("can-_ecu_762.csv")
 
 
 # Removing columns that contains null data
-df = df.dropna(axis=1)
+df.dropna(axis=1)
 
 
 # Removing columns where all sensors contain the same values
-df = df.loc[:, df.nunique() > 1]
+df.loc[:, df.nunique() > 1]
 
 print(df.info())
 
-df = df.rename(columns={
-    "timestamp": "Seconds",
-    "Analog Input #1": "RPM",
-    "Analog Input #2": "TPS"
-})
 
-df = df.dropna(axis=1)
-df = df.loc[:, df.nunique() > 1]
-
-print(df.info())
 
 
 # Plotting RPM and TPS vs Time graph
-fig = px.line(df, x="Seconds", y=["RPM", "TPS"], title="RPM and TPS vs Time")
+fig = px.line(df, x="timestamp", y=["RPM", "TPS"], title="RPM and TPS vs Time")
 fig.show()
