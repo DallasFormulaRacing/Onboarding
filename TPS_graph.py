63845f288dcdaaf9f62ca2bd1f46ec5675d38008
Zoë Bryant <thezoebryant@gmail.com>
2025-09-11 19:05:15 -0500

    Update TPS_graph.py

--- TPS_graph.py
+++ TPS_graph.py
@@ -2,15 +2,7 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv('dfr_data.csv',header = None)
-#df.dropna(inplace=True)
 df.head()
-
-#t = df[0]
-#print(t)
-#print(df.to_string()) 
-#df = px.data.gapminder().query("run=='Run #1'")
-#fig = px.line(df, x="timestamp", y="RMP", title='RPM over time')
-#fig.show()
 
 fig = px.line(df, x=df[0], y=df[18], title="RPM over time") 
 fig.update_layout(autotypenumbers='convert types')
