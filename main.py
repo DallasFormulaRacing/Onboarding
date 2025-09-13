f72e5d1c8ff31ad5e68d3f1f215ffcbfaaff9aec
Zain Karim <zain@zainkarim.com>
2025-09-12 22:42:02 -0500

    Plotted data for TPS and RPM to respective graphs.

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import pandas
-import plotly
+import plotly.express
 
 # Read CSV into a Pandas DF
 df = pandas.read_csv('can-_ecu_762.csv')
 
-# Remove null and non-working sensor data
-df = df.dropna(axis = 1, how = all)
+# Drop columns with null and/or repeated sensor data
+df = df.dropna(axis = 1, how = "all")
 df = df.loc[:, df.nunique(dropna = True) > 1]
 
 # Create graph with Plotly to show change in RMP and TPS over time
+df = df.reset_index().rename(columns={"index": "Time (samples)"})
+
+plotly.express.line(df, x="Time (samples)", y="RPM", title="RPM over Samples").show()
+plotly.express.line(df, x="Time (samples)", y="TPS", title="TPS over Samples").show()
