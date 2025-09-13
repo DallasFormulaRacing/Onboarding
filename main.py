fe9e138b64fdf0b4d6dc7bc79d2c7ef4fb65e736
Zain Karim <zain@zainkarim.com>
2025-09-12 22:26:02 -0500

    Dropped null and repeated data

--- main.py
+++ main.py
@@ -4,9 +4,8 @@ import plotly
 # Read CSV into a Pandas DF
 df = pandas.read_csv('can-_ecu_762.csv')
 
-print(df.head)
-
 # Remove null and non-working sensor data
+df = df.dropna(axis = 1, how = all)
+df = df.loc[:, df.nunique(dropna = True) > 1]
 
 # Create graph with Plotly to show change in RMP and TPS over time
-
