50b7f6008f765d7c55aa7a22595375240159ba21
Zoe Bryant <thezoebryant@gmail.com>
2025-09-15 17:59:25 -0500

    dropping empty analog inputs

--- TPS_graph.py
+++ TPS_graph.py
@@ -2,8 +2,11 @@ import pandas as pd
 import plotly.express as px
 
 df = pd.read_csv('dfr_data.csv',header = None)
-df.head()
+df.dropna().head()
 
-fig = px.line(df, x=df[0], y=df[18], title="TPS over time") 
+dead_analog = [col for col in df if df[col].empty == True]
+df = df.drop(columns=dead_analog)
+
+fig = px.line(df, x=df[0], y=df[18], title="RPM over time", labels={"x":"Time", "y":"RPM"}) 
 fig.update_layout(autotypenumbers='convert types')
 fig.show()
