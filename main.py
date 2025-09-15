2b6cd121ce563593e7c2f680412a1627b0495d3f
jc-rami4 <johnrami0310@gmail.com>
2025-09-15 17:12:24 -0500

    created psuedocode

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+# Read CSV via pandas
+# Change both RPM and TPS to Arrays
+# Make a for loop
+# Run RPM and TPS through that loop, removing null values
+# Take those values and graph it via Plotly
+
+import pandas as pd
+
 print("hello world my changes")
