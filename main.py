6c764efae1bf67d0bc7045e8776e925b36cee633
Timo Kamgang <timokamgang@gmail.com>
2025-09-15 21:33:45 -0500

    updated changes to close pr

--- main.py
+++ main.py
@@ -8,16 +8,14 @@ df = pd.read_csv("data.csv")
 df.columns = df.columns.str.strip()
 
 #clean the data who have all same values in rows or NAN values
-df = df.dropna(axis=1).loc[:, lambda x: x.nunique() > 1]
+df = df.loc[:, lambda x: x.nunique() > 1]
 
-#keep the graphed data to the first 100 rows so that the graph can render
-df_sampled = df.head(100)
 
 #graph the data (RPM over time)
-fig_rpm = px.line(df_sampled, x='timestamp', y = 'RPM', title="RPM over Time", labels={"timestamp":"Time", ' RPM': "RPM"})      
+fig_rpm = px.line(df, x='timestamp', y = 'RPM', title="RPM over Time", labels={"timestamp":"Time", 'RPM': "RPM"})      
 
 #graph TPS over time 
-fig_tps = px.line(df_sampled, x = 'timestamp', y = 'TPS', title = "TPS over time", labels={"timestamp":"Time", " TPS": "TPS"})
+fig_tps = px.line(df, x = 'timestamp', y = 'TPS', title = "TPS over time", labels={"timestamp":"Time", "TPS": "TPS"})
 
 #show the graphs
 fig_rpm.show()
