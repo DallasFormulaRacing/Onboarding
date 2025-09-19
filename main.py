95eebe1ff7e9e95b649e1645bf461265d3df4ec3
N0tgokul <149739814+N0tgokul@users.noreply.github.com>
2025-09-19 16:36:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 import plotly.express as px
 
-df = pd.read.csv(can_data.csv)
+df = pd.read_csv("can_data.csv")
 
 for col in df.columns:
   if "Analog Input" in col and df[col].nunique() ==1:
-    df.drop(col, axis = 1, inplace = true)
+    df.drop(col, axis = 1, inplace = True)
 
 fig_rpm = px.line(df, x = "Time", y = "RPM", title = "RPM over Time")
 fig_rpm.update_xaxes(title = "Time")
