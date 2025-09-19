0da14323761c2228915d0116712ca13d7ba15e90
kimbow231 <80603257+kimbow231@users.noreply.github.com>
2025-09-19 16:46:34 -0500

    Plotted TPS and RPM on graph

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px
+
+#reads the data
+data = pd.read_csv('can_data.csv')
+
+#runs through all the columns and if they repeat the same input and are named alalog input they are dropped
+for c in data.columns:
+    if "Analog Input" in c and data[c].nunique() == 1:
+        data.drop(columns = c, inplace = True)
+        print("deleted" + c)
+
+print(data.columns)
+
+#creates a graph plotting two lines of TPS and RPM over time
+fig = px.line(data, x = "timestamp", y = ["TPS","RPM"], title = "TPS and RPM over time")
+fig.show()
+
