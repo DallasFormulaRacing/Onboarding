b2c978ebc808a73c18576791705141fdaa10c50e
N0tgokul <149739814+N0tgokul@users.noreply.github.com>
2025-09-19 16:57:23 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ for col in df.columns:
   if "Analog Input" in col and df[col].nunique() ==1:
     df.drop(col, axis = 1, inplace = True)
 
-fig_rpm = px.line(df, x = "Time", y = "RPM", title = "RPM over Time")
-fig_rpm.update_xaxes(title = "Time")
+fig_rpm = px.line(df, x = "timestamp", y = "RPM", title = "RPM over Time")
+fig_rpm.update_xaxes(title = "Timestamp")
 fig_rpm.update_yaxes(title = "RPM")
 fig_rpm.show()
 
-fig_tps = px.line(df, x = "Time", y = "TPS", title = "TPS over Time")
-fig_tps.update_xaxes(title = "Time")
-fig_tps.update_yaxes(title = "Throttle position Sensor(TPS)")
+fig_tps = px.line(df, x = "timestamp", y = "TPS", title = "TPS over Time")
+fig_tps.update_xaxes(title = "Timestamp")
+fig_tps.update_yaxes(title = "Throttle Position Sensor(TPS)")
 fig_tps.show()
 
