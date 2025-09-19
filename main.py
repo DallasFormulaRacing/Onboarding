a328ea910e6c507b372bd65a21e0494d356496f6
reversal312 <senbala321@gmail.com>
2025-09-19 14:29:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px
+import matplotlib.pyplot as plt 
+df = pd.read_csv("can_data.csv")
+
+for col in df.columns:
+    if "Analog Input" in col and df[col].nunique() == 1:
+        df.drop(columns=[col], inplace=True)
+
+plt.figure(figsize=(10,6)) 
+plt.plot(df['timestamp'], df['RPM'], label='RPM', color='blue')
+plt.plot(df['timestamp'], df['TPS'], label='TPS', color='red')
+
+plt.title('RPM and TPS over Time')
+plt.xlabel('timestamp')
+plt.ylabel('RPM and TPS values')
+plt.legend()
+plt.grid(True)
+plt.show()
