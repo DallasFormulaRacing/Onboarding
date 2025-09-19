aff2d81115e5cb56025717db3ac0eba91bba6cc3
N0tgokul <149739814+N0tgokul@users.noreply.github.com>
2025-09-18 23:18:54 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
-print("hello world my changes")
+import pandas as pd
+import plotly.express as px
+
+df = pd.read.csv(can_data.csv)
+
+for col in df.columns:
+  if "Analog Input" in col and df[col].nunique() ==1:
+    df.drop(col, axis = 1, inplace = true)
+
+fig_rpm = px.line(df, x = "Time", y = "RPM", title = "RPM over Time")
+fig_rpm.update_xaxes(title = "Time")
+fig_rpm.update_yaxes(title = "RPM")
+fig_rpm.show()
+
+fig_tps = px.line(df, x = "Time", y = "TPS", title = "TPS over Time")
+fi_tps.update_xaxes(title = "Time")
+fig_tps.update_yaxes(title = "Throttle position Seonsor(TPS)")
+fig_tps.show()
+
